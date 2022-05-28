62899e9359cfc89c9d4692f3074e22517748e69c
mdshahin-ahmed <mdshahinahmed605701@gmail.com>
2022-05-28 22:21:53 +0600

    Done

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -2,7 +2,7 @@ import { SafeAreaView, StyleSheet, Image, ScrollView } from "react-native";
 import Text from "../Components/Text/Text";
 import { colors } from "../Theme/Colors";
 import { View } from "react-native-web";
-import { Feather, AntDesign } from "@expo/vector-icons";
+import { Feather, AntDesign, Ionicons } from "@expo/vector-icons";
 import { spacing } from "../Theme/Spacing";
 
 export default function Home({ navigation }) {
@@ -41,11 +41,97 @@ export default function Home({ navigation }) {
           </Text>
           <View style={styles.buttonWrap}>
             <View style={styles.callButton}>
-              <Text>Call Now</Text>
+              <Ionicons name="call" size={24} color="white" />
+              <Text
+                style={{
+                  color: colors.white,
+                  fontWeight: "bold",
+                  marginLeft: 10,
+                }}
+              >
+                Call Now
+              </Text>
             </View>
             <View style={styles.smsButton}>
-              <Text>Send SMS</Text>
+              <Feather name="message-circle" size={24} color="white" />
+              <Text
+                style={{
+                  color: colors.white,
+                  fontWeight: "bold",
+                  marginLeft: 10,
+                }}
+              >
+                Send SMS
+              </Text>
             </View>
+          </View>
+        </View>
+        <View style={styles.main}>
+          <Text
+            preset="h2"
+            style={{
+              marginTop: 31,
+              color: colors.purpleText,
+              fontWeight: "bold",
+              marginBottom: 25,
+            }}
+          >
+            Prevention
+          </Text>
+        </View>
+        <View style={styles.mainImageWrap}>
+          <View style={styles.imageTextWrap}>
+            <Image
+              style={styles.mainImage}
+              source={require("../images/XMLID_1.png")}
+            ></Image>
+            <Text
+              preset="h4"
+              style={{ color: colors.purpleText, textAlign: "center" }}
+            >
+              Avoid close contact
+            </Text>
+          </View>
+          <View style={styles.imageTextWrap}>
+            <Image
+              style={styles.mainImage}
+              source={require("../images/XMLID_2.png")}
+            ></Image>
+            <Text
+              preset="h4"
+              style={{ color: colors.purpleText, textAlign: "center" }}
+            >
+              Clean your hands often
+            </Text>
+          </View>
+          <View style={styles.imageTextWrap}>
+            <Image
+              style={styles.mainImage}
+              source={require("../images/XMLID_3.png")}
+            ></Image>
+            <Text
+              preset="h4"
+              style={{ color: colors.purpleText, textAlign: "center" }}
+            >
+              Wear a facemask
+            </Text>
+          </View>
+        </View>
+        <View style={styles.test}>
+          <Image
+            style={styles.testImage}
+            source={require("../images/Group.png")}
+          ></Image>
+          <View style={styles.testTextWrap}>
+            <Text
+              preset="h3"
+              style={{ fontWeight: "bold", color: colors.white }}
+            >
+              Do your own test!
+            </Text>
+            <Text preset="small" style={styles.testSmall}>
+              Follow the instructions to do your own test.
+            </Text>
           </View>
         </View>
       </ScrollView>
@@ -57,6 +143,8 @@ const styles = StyleSheet.create({
   banner: {
     backgroundColor: colors.purple,
     paddingHorizontal: spacing[6],
+    borderBottomRightRadius: spacing[8],
+    borderBottomLeftRadius: spacing[8],
   },
   menuWrap: {
     marginTop: 50,
@@ -99,17 +187,68 @@ const styles = StyleSheet.create({
   buttonWrap: {
     flexDirection: "row",
     justifyContent: "space-between",
+    marginVertical: 31,
   },
   callButton: {
     backgroundColor: colors.pink,
-    paddingHorizontal: spacing[10],
-    paddingVertical: spacing[5],
+    paddingHorizontal: spacing[6],
+    paddingVertical: spacing[4],
     borderRadius: spacing[7],
+    flexDirection: "row",
+    alignItems: "center",
   },
   smsButton: {
     backgroundColor: colors.blue,
-    paddingHorizontal: spacing[10],
-    paddingVertical: spacing[5],
+    paddingHorizontal: spacing[6],
+    paddingVertical: spacing[4],
     borderRadius: spacing[7],
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  main: {
+    paddingHorizontal: spacing[5],
+  },
+  mainImageWrap: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  imageTextWrap: {
+    flex: 1,
+    marginHorizontal: 20,
+  },
+  mainImage: {
+    flex: 1,
+    height: 88,
+    width: 88,
+    resizeMode: "container",
+    marginHorizontal: "auto",
+    marginBottom: spacing[3],
+  },
+  test: {
+    marginHorizontal: spacing[6],
+    backgroundColor: colors.purple,
+    flexDirection: "row",
+    marginTop: 44,
+    paddingVertical: spacing[6],
+    paddingHorizontal: spacing[3],
+    borderRadius: spacing[3],
+    marginBottom: 90,
+    // background: "linear-gradient(colors.blue, colors.pink)",
+  },
+  testImage: {
+    flex: 1,
+    marginTop: -35,
+    height: 110,
+    width: 115,
+    resizeMode: "contain",
+  },
+  testTextWrap: {
+    flex: 4,
+  },
+  testSmall: {
+    fontWeight: 400,
+    lineHeight: 22,
+    marginTop: spacing[3],
+    opacity: 0.8,
   },
 });
